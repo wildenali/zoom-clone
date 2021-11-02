4d2dcbb42213913fae0a6f7d5e10529293c8770f
rc-wilden <rc-wilden@dewatic.com>
2021-11-02 11:42:28 +0700

    stop play video

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ let myVideoStream
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audia: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -81,4 +81,60 @@ const addVideoStream = (video, stream) => {
 const scrollToBottom = () => {
     let d = $('.main__chat_window');
     d.scrollTop(d.prop("scrollHeight"));
+}
+
+// Mute
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if (enabled){
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myVideoStream.getAudioTracks()[0].enabled = true
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+        <i class="fas fa-microphone"></i>
+        <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+        <i class="unmute fas fa-microphone-slash"></i>
+        <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const playStop = () => {
+    console.log('object paly stop');
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setStopVideo = () => {
+    const html = `
+        <i class="fas fa-video"></i>
+        <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+        <i class="stop fas fa-video-slash"></i>
+        <span>Play  Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
 }
